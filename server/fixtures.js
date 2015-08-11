312f915140fc96382200676c19fd23d54a8cafec
lukesang <lukesang@gmail.com>
2015-08-11 15:06:41 +0800

    collection
    Creat mongodb that it can be utilzed

---
+++ server/fixtures.js
@@ -0,0 +1,21 @@
+if (Posts.find().count() === 0) {
+  Posts.insert({
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  });
+
+  Posts.insert({
+    title: 'Meteoraaaaaaa',
+    url: 'http://meteor.com'
+  });
+
+  Posts.insert({
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  });
+}
+Posts.insert({
+  title: 'Meteor Docsa',
+  author: 'Tom Coleman', 
+  url: 'http://docs.meteor.com'
+});
